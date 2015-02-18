9821c281bc0fdabb105dffa9d9e6e13bd4cbaafd
Allen <agvawd@hotmail.com>
2015-02-17 20:47:48 -0700

    Completed Steps 1-5

--- js/app.js
+++ js/app.js
@@ -4,4 +4,26 @@ app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
   //router here
+  $routeProvider
+  .when("/",{
+  	templateUrl: "/js/home/homeTmpl.html",
+  	controller: "homeCtrl",
+/*  	resolve: {
+  		teamService: function(homeService){
+  			return 
+  		}
+  	}*/
+  })
+  .when("/teams/:team",{
+  	templateUrl: "/js/teams/teamTmpl.html",
+  	controller: "teamCtrl",
+  	resolve: {
+  		teamData: function($route, teamService){
+  			return teamService.getTeamData($route.current.params.team);
+  		}	
+  	}
+  })
+  .otherwise({
+  	redirectTo: "/"
+  })
 });
--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,45 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+	$scope.teamData = teamData;
 
+	$scope.newGame = {};
+
+	$scope.showNewGameForm = false;
+	$scope.toggleNewGameForm = function() {
+		$scope.showNewGameForm = !$scope.showNewGameForm;
+	}
+
+	$scope.formatDate = function(dateStr) {
+		return new Date(dateStr).toLocaleDateString();
+	}	
+
+	if($routeParams.team === "utahjazz") {
+		$scope.homeTeam = "Utah Jazz";
+		$scope.logoPath = "images/jazz-logo.png"
+	}
+	else if($routeParams.team === "miamiheat"){
+		$scope.homeTeam = "Miami Heat";
+		$scope.logoPath = "images/heat-logo.png"
+	}
+	else if($routeParams.team === "losangeleslakers"){
+		$scope.homeTeam = "Los Angeles Lakers";
+		$scope.logoPath = "images/lakers-logo.png"
+	}
+
+	$scope.submitGame = function() {
+		$scope.newGame.homeTeam = $scope.homeTeam.split(' ').join('').toLowerCase();
+		$scope.newGame.homeTeamScore = $scope.homeScore;
+		$scope.newGame.opponent = $scope.mySelected;
+		$scope.newGame.opponentScore = $scope.awayScore;
+		teamService.addNewGame($scope.newGame).then(function(data){
+			teamService.getTeamData($scope.newGame.homeTeam).then(function(data){
+				$scope.teamData = data;
+				$scope.newGame = {};
+				$scope.showNewGameForm = false;
+			});
+		});
+	}
+
+		console.log($scope.teamData);
 });
